23b247984719b9d9808fa39d167a06382c400f74
Rishat <rishatl@inbox.ru>
2021-09-20 18:52:55 +0300

    Initial project

---
+++ DispatchExample/DispatchExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DispatchExample
+//
+//  Created by Rishat on 20.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
